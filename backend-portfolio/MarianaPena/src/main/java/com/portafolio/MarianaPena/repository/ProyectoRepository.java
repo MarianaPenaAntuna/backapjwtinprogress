602ad516f9ec0d91c0dbbb745913d9d1b14f0c9d
MarianaPenaAntuna <98125793+MarianaPenaAntuna@users.noreply.github.com>
2022-12-01 13:26:22 -0300

    .

---
+++ backend-portfolio/MarianaPena/src/main/java/com/portafolio/MarianaPena/repository/ProyectoRepository.java
@@ -0,0 +1,10 @@
+
+package com.portafolio.MarianaPena.repository;
+
+import com.portafolio.MarianaPena.model.Proyecto;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+
+public interface ProyectoRepository extends JpaRepository <Proyecto,Long>{
+    
+}
---
+++ backend-portfolio/MarianaPena/src/main/java/com/portafolio/MarianaPena/repository/SkillRepository.java
@@ -0,0 +1,10 @@
+
+package com.portafolio.MarianaPena.repository;
+
+import com.portafolio.MarianaPena.model.Skill;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+
+public interface SkillRepository extends JpaRepository <Skill,Long>{
+    
+}
